719a398d13d575c2bca5e5a58c9abdf5e9e1551c
Thomas Ochman <thomas@dragster.se>
2022-03-13 07:07:51 +0100

    rearranges specs and adds soec for request body

--- cypress/integration/submitInterestForm.feature.js
+++ cypress/integration/submitInterestForm.feature.js
@@ -1,26 +1,31 @@
 /* eslint-disable no-undef */
 describe("navigating to the acttivity view", () => {
-  before(() => {
-    cy.intercept(
-      "POST",
-      "https://hooks.slack.com/services/**",
-      { body: "ok", statusCode: 200 }
-    ).as("postSlack");
+  beforeEach("and filling in both fields", () => {
+    cy.intercept("POST", "https://hooks.slack.com/services/**", {
+      body: "ok",
+      statusCode: 200,
+    }).as("postSlack");
     cy.visit("/agenda");
-  });
-
-  before("and filling in both fields", () => {
     cy.get("[data-cy=name-input]").type("Thomas");
     cy.get("[data-cy=email-input]").type("thomas@email.com");
-    cy.get("[data-cy=check-1]").click()
-    cy.get("[data-cy=check-2]").click()
+    cy.get("[data-cy=check-1]").click();
+    cy.get("[data-cy=check-2]").click();
     cy.get("[data-cy=send-button]").click();
   });
 
   it("is expected to make a POST request", () => {
-    cy.wait("@postSlack")
-      .its("request.method")
-      .should("eq", 'POST')
+    cy.wait("@postSlack").its("request.method").should("eq", "POST");
+  });
+
+  it("is expected to send a message", () => {
+    const expectedMessageAttributes = {
+      icon_emoji: ":pencil2:",
+      text: "Has submitted an interest form for Läs och Förstå 1700-talet and Vitterhetsbal.\nUse thomas@email.com to get in touch.",
+      username: "Thomas",
+    };
+    cy.wait("@postSlack").then((interception) => {
+      expect(interception.request.body).to.eql(expectedMessageAttributes)
+    });
   });
 
   it("is expected to hide form", () => {
